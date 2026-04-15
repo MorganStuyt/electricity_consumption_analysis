7520d66f5b5bd51260e4938176ae7e788d466b65
Morgan Stuyt <54088799+MorganStuyt@users.noreply.github.com>
2026-04-15 22:35:36 +1200

    Create analyse.py
    file creation and first few lines put down

---
+++ analyse.py
@@ -0,0 +1,13 @@
+import matplotlib
+import pandas as pd
+
+# pip install matplotlib pandas
+
+
+def load_data(datasource, existingfile):
+    # import from csv or another data source and print the head
+
+    #
+
+existingfile = "filename.parquet"
+load_data(file, existingfile = 1)
