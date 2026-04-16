1f923fba2f6c18533a108fcde5745f9120aaf524
Morgan Stuyt <54088799+MorganStuyt@users.noreply.github.com>
2026-04-16 22:21:32 +1200

    Data and analysis script updates
    Added a csv data file and got the analysis script to load the csv, clean it up, and create a time series plot with the consumption

--- analyse.py
+++ analyse.py
@@ -1,13 +1,34 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # pip install matplotlib pandas
 
 
-def load_data(datasource, existingfile):
+def load_data(newdata, existingfile):
     # import from csv or another data source and print the head
+    
+    #skip loading the header row HDR, as it only contains data relating to the creation of the file
+    df = pd.read_csv(newdata, skiprows=1)
+    
+    #creating a new dataframe with only the required columns, then adding column labels
+    new_df = df.iloc[:, [9,10,12]].copy()
+    new_df.columns = ['Start Date', 'End Date', 'Consumption']
+    new_df['Start Date'] = pd.to_datetime(new_df['Start Date'])
+    #print(new_df.head())
 
-    #
+    return new_df
 
-existingfile = "filename.parquet"
-load_data(file, existingfile = 1)
+def plot_data(data):
+    df = data.dropna()
+
+    fig, ax = plt.subplots()
+
+    ax.plot(df['Start Date'], df['Consumption'], marker='o')
+
+    plt.xlabel('Date')
+    plt.ylabel('Consumption [kWh]')
+    plt.show()
+
+newdata = r'C:\git\electricity_consumption_analysis\data\CTCT_E_CUST_ICPCONS_202604_20260416_0235950955LCB3520260416125901.CSV'
+data = load_data(newdata, existingfile = 1)
+plot_data(data)
